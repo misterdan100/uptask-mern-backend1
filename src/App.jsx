eaeb7c39cfce1f5224678800c3b56527d5e59447
dandanarq <merchanmd@gmail.com>
2024-04-02 23:12:09 -0500

    chore: create public routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
 
+import Login from "./paginas/Login";
+import Registrar from "./paginas/Registrar";
+import OlvidePassword from "./paginas/OlvidePassword";
+import NuevoPassword from "./paginas/NuevoPassword";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 
 function App() {
 
-
   return (
-    <div className="app">
-      <h1>UpTask</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} /> 
+          <Route path="registrar" element={<Registrar />} /> 
+          <Route path="olvide-password" element={<OlvidePassword />} /> 
+          <Route path="nuevo-password/:token" element={<NuevoPassword />} /> 
+          <Route path="confirmar/:id" element={<ConfirmarCuenta />} /> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
