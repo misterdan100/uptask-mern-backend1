cef6275de2ff537c0d0a1b2991887c5cb779f05a
dandanarq <merchanmd@gmail.com>
2024-04-08 21:14:40 -0500

    fix: route about olvide-password with token

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <Route index element={<Login />} /> 
           <Route path="registrar" element={<Registrar />} /> 
           <Route path="olvide-password" element={<OlvidePassword />} /> 
-          <Route path="nuevo-password/:token" element={<NuevoPassword />} /> 
+          <Route path="olvide-password/:token" element={<NuevoPassword />} /> 
           <Route path="confirmar/:id" element={<ConfirmarCuenta />} /> 
         </Route>
       </Routes>
