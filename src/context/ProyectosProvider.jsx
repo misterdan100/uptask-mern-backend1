c3a61419e283c93bc57b3f4f263af56fa0ff54c6
dandanarq <merchanmd@gmail.com>
2024-04-15 22:36:08 -0500

    chore: create projects context w/ provider and hook

---
+++ src/context/ProyectosProvider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState, useEffect } from "react"
+import clienteAxios from "../config/clienteAxios"
+
+const ProyectosContext = createContext()
+
+const ProyectosProvider = ({children}) => {
+  const [ proyectos, setProyectos ] = useState([])
+
+
+  return (
+    <ProyectosContext.Provider
+    value={{
+      proyectos,
+      
+    }}
+    >
+      {children}
+    </ProyectosContext.Provider>
+  )
+}
+
+export default ProyectosContext
+export {ProyectosProvider}
+
